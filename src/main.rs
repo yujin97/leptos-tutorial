21a84c07fd582e27579766b830f2e7384a9da3d5
Eugene <eugene97@connect.hku.hk>
2023-10-02 23:07:24 +0900

    feat: create view from iteration

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ fn App() -> impl IntoView {
         </button>
         <ProgressBar progress=count/>
         <ProgressBar progress=Signal::derive(double_count)/>
+        <StaticViewList/>
+        <DynamicItems length=10/>
     }
 }
 
@@ -34,4 +36,40 @@ fn ProgressBar(
     progress: Signal<i32>,
 ) -> impl IntoView {
     view! { <progress max=max value=progress></progress> }
+}
+
+#[component]
+fn StaticViewList() -> impl IntoView {
+    let values = vec![0, 1, 2];
+    view! {
+        <p>{values.clone()}</p>
+        <ul>
+            {values.into_iter()
+                .map(|n| view! {<li>{n}</li>})
+                .collect::<Vec<_>>()}
+        </ul>
+    }
+}
+
+#[component]
+fn DynamicItems(length: usize) -> impl IntoView {
+    let counters = (1..=length).map(|idx| create_signal(idx));
+
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button
+                        on:click= move|_| set_count.update(|n| *n += 1)
+                    >
+                        {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! {
+        <ul>{counter_buttons}</ul>
+    }
 }
